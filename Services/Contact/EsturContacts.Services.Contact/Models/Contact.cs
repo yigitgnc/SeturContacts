a763ea7e842cb25f2a225aa8b826c3d96de0d815
yiğit genç <33726177+yigitgnc@users.noreply.github.com>
2022-03-10 21:18:13 +0300

    saying hello to git dev branch

---
+++ Services/Contact/EsturContacts.Services.Contact/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace EsturContacts.Services.Contact.Models
+{
+    public class Contact
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Guid Id { get; set; }
+        public string UserID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+        public ContactInfo ContactInformation { get; set; }
+
+
+    }
+}
