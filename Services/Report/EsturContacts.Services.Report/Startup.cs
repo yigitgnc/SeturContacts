d33210d034f21e6c2fd6106a5b1530ac7f00592b
yiğit genç <33726177+yigitgnc@users.noreply.github.com>
2022-03-11 12:13:58 +0300

    rabbitmq config

--- Services/Report/EsturContacts.Services.Report/Startup.cs
+++ Services/Report/EsturContacts.Services.Report/Startup.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,18 @@ namespace SeturContacts.Services.Report
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x=>
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
+                      {
+                          host.Username("guest");
+                          host.Password("guest");
+                      });
+                });
+            });
+
             services.AddScoped<IReportService, ReportService>();
 
             services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
