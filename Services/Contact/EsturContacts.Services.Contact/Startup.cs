2f2c5c343a1c861c5a5f2724ad8333b9d2f40fbe
yiğit genç <33726177+yigitgnc@users.noreply.github.com>
2022-03-11 05:19:49 +0300

    docker mongo db continer eklendi

--- Services/Contact/EsturContacts.Services.Contact/Startup.cs
+++ Services/Contact/EsturContacts.Services.Contact/Startup.cs
@@ -58,7 +58,7 @@ namespace EsturContacts.Services.Contact
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
